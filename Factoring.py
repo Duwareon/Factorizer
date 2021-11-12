5e4ecb30834844961721130fd621cfb1a7280dae
duwareon <duwareon@gmail.com>
2021-11-12 13:22:46 -0500

    finished difference of squares though it shows things as sqrt(x**2)

--- Factoring.py
+++ Factoring.py
@@ -4,31 +4,54 @@ init_printing(use_unicode=True)
 
 #x, y = symbols('x y')
 
-#polynomstr = "9*x**2-49"#*x"
-polynomstr = "6*(x**2) + 6*(x+1)"
+#polynomstr = "9*x**2-49"
+polynomstr = "9*x**2-25*y**2"
 
 polynomial = parse_expr(polynomstr, evaluate=False)
 
 
 
 def nfactor(polynomial):
-    # Check for the greatest common denominator.
+    # set up some useful variables.
     separateterms = Add.make_args(polynomial)
     numofterms = len(separateterms)
-    #return numofterms, separateterms
     denom = gcd(separateterms)
-    #squarediff = checksquarediff(polynomial)
-    #pprint(separateterms)
+    #pprint(denom)
+    
 
     # If the gcd isn't 1, factor the expression. I'm using factor() here because if I multiply it, it'll display wrong sometimes.
     # I do have the correct math done, it just tries to make the equation look nice if I multiply it like normal and it makes it not factored anymore.
+    # It's either this or having it display certain things as 3*(4/3)
     if denom != 1:
         return (factor(denom * (polynomial / denom)))
 
-    """
-    elif numofterms == 2 & (separateterms[-1]/-1)<0:
-        print("pog")
-        return sqaurediff(polynomial)
+    # Check if the polynomial is a binomial
+    elif numofterms == 2:
+        # Check if the binomial is able to be square rooted
+        if perfectsquare(polynomial):
+            
+            # Check if the second term is a monomial or not
+            # The check happens because it's easier to check if the second term is negative this way
+            isNomial = True
+            try:
+                Poly(separateterms[1])
+            except:
+                isNomial = False
+            
+            
+            if isNomial:
+                # Check if the second term is negative
+                if any(ele < 0 for ele in Poly(separateterms[-1]).all_coeffs()):
+                    return squarediff(polynomial)
+                
+                else: return False
+                
+            else:
+                if (separateterms[1]/-1)>0:
+                    return squarediff(polynomial)
+                else: return False
+        else:
+            pass
     
     elif numofterms == 3:
         pass
@@ -36,18 +59,23 @@ def nfactor(polynomial):
     elif numofterms == 4:
         pass
     
+    return False
+    
 
-    else:
-        return None
-    """ 
-
-def sqaurediff(polynomial):
+def perfectsquare(polynomial):
+    args = Add.make_args(polynomial)
+    return True
+    
+def squarediff(polynomial):
     #a**2 + b**2 = (a+b)(a-b)
     args = Add.make_args(polynomial)
-    #polynomial = Poly(polynomial)
-    return Poly(args[0]), Poly(LT(args[0])).all_coeffs()
-    #return Pow(sqrt(args[0].coeff(x, 2)), 2, evaluate=False)
+
+    # TODO: make this not display as sqrt(x**2)
+    a = (sqrt(args[0]))
+    b = sqrt(abs(args[1]))
+
+    return ((a+b)*(a-b))
 
 pprint(nfactor(polynomial))
-
-
+pprint(factor(polynomial))
+pprint(sqrt(6**2) == 6) 
